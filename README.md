b917fc988e7889aeef82aa0b0b3b5b88989374af
Ally <allysondacruz201@gmail.com>
2026-05-19 10:05:10 -0300

    Adding README an update testCases

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # estrategia-educacional-pp
-Projeto Pessoal 
+
+     Projeto Pessoal 
+
+# Projeto QA - Estratégia Educacional
+
+Projeto de testes automatizados utilizando Cypress.
+
+## 🔧 Tecnologias
+- Cypress
+- JavaScript
+
+## 🧪 Testes
+- Login
+- Navegação
+- Validação de elementos
+
+## 🌐 Aplicação testada
+https://perfil.estrategia.com/
--- cypress/e2e/testCases.spec.cy.js
+++ cypress/e2e/testCases.spec.cy.js
@@ -6,16 +6,29 @@ const home = new homePage()
 
 describe('Tests Case - Estratégia Educacional', () => {
   
-  it('Pesquisa de curso  QA', () => {
+
+  it('Looking for Teachers - Check Prices - and more', () => {
     home.accesshomePage()
+    cy.get('[href="https://www.estrategiaconcursos.com.br/cursos/professor/"]').click()
+    cy.get('.page-header-inner').should('be.visible')
+    cy.get(':nth-child(4) > .card-prod-title > a', {timeout: 10000}).click({ force: true })
+    cy.get(':nth-child(1) > .card-prod-details').click()
+    cy.get('.page-header-inner').should('be.visible', 'contain', 'ANS (Especialista em Regulação de Saúde Suplementar) Português')
+    cy.get('.cur-details-info > .guarantee-box > .guarantee-box-content > a').click()
+    cy.get("[aria-label='Close']").click()
+    cy.get('.featherlight').click()
+    cy.get("[alt='Logo - Estratégia Concursos']").click()
     cy.get('.layout-header-search > .search-input > [name="q"]').click().type('Curso de QA')
     cy.get("[type='submit'] .icon-find").click()
-  })
+    cy.get("[alt='Logo - Estratégia Concursos']").click()
 
-  it('Search by Teacher', () => {
-    home.accesshomePage()
-   
+ 
 })
 
-}
-)
+
+  })
+
+ 
+   
+
+
